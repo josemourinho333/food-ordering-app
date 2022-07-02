6f7a37ab0b61283b7a2ba46f38a968f78633b281
ukflava <ukflava@gmail.com>
2022-07-02 12:21:19 -0700

    new layout for tables database

---
+++ db/schema/02_dblayout .sql
@@ -0,0 +1,51 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS menu_items CASCADE;
+DROP TABLE IF EXISTS total_orders CASCADE;
+DROP TABLE IF EXISTS order_items CASCADE;
+
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  is_admin BOOLEAN NOT NULL
+  phone_number VARCHAR(255) NOT NULL
+  email VARCHAR(255) NOT NULL,
+  street_address VARCHAR(255) NOT NULL
+  province VARCHAR(255) NOT NULL
+  country VARCHAR(255) NOT NULL
+  postal_code VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE menu_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  price INTEGER NOT NULL,
+  photo_url VARCHAR(255) NOT NULL,
+);
+
+-- currently not used
+-- CREATE TABLE menu (
+-- menu_item_id INTEGER REFERENCES menu_items(id) ON DELETE CASCADE,
+-- );
+
+
+CREATE TABLE total_orders (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  status_sent boolean NOT NULL,
+  status_finished boolean NOT NULL,
+  time_sent DATE NOT NULL,
+  time_confirmed DATE NOT NULL,
+  time_fulfilled DATE NOT NULL,
+  time_of_pickup DATE NOT NULL,
+);
+
+CREATE TABLE order_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  total_orders_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  menu_item_id INTEGER REFERENCES menu_items(id) ON DELETE CASCADE,
+  quantity INTEGER NOT NULL
+ );
+
+
---
+++ db/seeds/02_initialseeds.sql
@@ -0,0 +1,14 @@
+-- INSERT INTO users (
+--     username, is_admin, phone_number, email, street_address, province, country, postal_code)
+--     VALUES (
+--     'Sample Name', 'sample@yahoo.com', 'password', true, '222-1234567', '123-1221 Noname str', 'BC', 'USA', '90210');
+
+--     INSERT INTO menu_items (
+--     name, description, price, photo_url,)
+--     VALUES (
+--     'Burger 1', 'Lorem Ipsum', 1000, 'www.google.com');
+
+-- INSERT INTO orders (
+--     user_id, menu_item_id, quantity, status, time_sent, time_confirmed, time_fulfilled)
+--     VALUES ( 1, 1, 1, true, 2022-07-01-00-00-00,  2022-07-01-00-00-01, 2022-07-01-00-00-02,
+--     );
