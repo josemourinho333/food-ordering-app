8f8ca347bbbaf85b2f141e9ab8aafece65020b8c
ukflava <ukflava@gmail.com>
2022-07-02 16:15:37 -0700

    change is_admin in seeds of DB

--- db/seeds/02_DB_seeds.sql
+++ db/seeds/02_DB_seeds.sql
@@ -3,8 +3,8 @@ VALUES
   ('Brennan Padilla','false','1-279-271-5628','ectus.quis@icloud.com','Ap #287-6384 Eu St.','Prince Edward Island','Canada','61C 5H7'),
   ('Belle Hendrick','true','1-334-375-1547','quis@hotmail.couk','177-3446 Donec St.','Quebec','Canada','T8P 8G7'),
   ('Vance Melton','false','(881) 663-0046','lorem@protonmail.com','Ap #243-9390 Erat, St.','Quebec','Canada','Q5B 0E9'),
-  ('Aquila Francis','true','1-482-761-0678','lorem.vitae@google.com','904-7492 Aliquam Avenue','New Brunswick','Canada','25T 0P1'),
-  ('Amanda Garner','true','(390) 131-1912','nam@aol.couk','Ap #566-6841 Pretium Av.','New Brunswick','Canada','L3K 2Y3');
+  ('Aquila Francis','false','1-482-761-0678','lorem.vitae@google.com','904-7492 Aliquam Avenue','New Brunswick','Canada','25T 0P1'),
+  ('Amanda Garner','false','(390) 131-1912','nam@aol.couk','Ap #566-6841 Pretium Av.','New Brunswick','Canada','L3K 2Y3');
 
     INSERT INTO menu_items (
     name, description, price, photo_url)
