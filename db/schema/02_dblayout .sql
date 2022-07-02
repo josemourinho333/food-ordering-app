cc6831074b6524aea211935ff545abf30a90ad5d
ukflava <ukflava@gmail.com>
2022-07-02 12:57:17 -0700

    change layout syntax with comma

--- db/schema/02_dblayout .sql
+++ db/schema/02_dblayout .sql
@@ -7,12 +7,12 @@ DROP TABLE IF EXISTS order_items CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  is_admin BOOLEAN NOT NULL
-  phone_number VARCHAR(255) NOT NULL
+  is_admin BOOLEAN NOT NULL,
+  phone_number VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
-  street_address VARCHAR(255) NOT NULL
-  province VARCHAR(255) NOT NULL
-  country VARCHAR(255) NOT NULL
+  street_address VARCHAR(255) NOT NULL,
+  province VARCHAR(255) NOT NULL,
+  country VARCHAR(255) NOT NULL,
   postal_code VARCHAR(255) NOT NULL
 );
 
@@ -21,7 +21,7 @@ CREATE TABLE menu_items (
   name VARCHAR(255) NOT NULL,
   description VARCHAR(255) NOT NULL,
   price INTEGER NOT NULL,
-  photo_url VARCHAR(255) NOT NULL,
+  photo_url VARCHAR(255) NOT NULL
 );
 
 -- currently not used
@@ -38,7 +38,7 @@ CREATE TABLE total_orders (
   time_sent DATE NOT NULL,
   time_confirmed DATE NOT NULL,
   time_fulfilled DATE NOT NULL,
-  time_of_pickup DATE NOT NULL,
+  time_of_pickup DATE NOT NULL
 );
 
 CREATE TABLE order_items (
