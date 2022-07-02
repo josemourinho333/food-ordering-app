582617c57d53b359b019bcdeec2b2283b0ee3365
josemourinho333 <yoo.phil92@gmail.com>
2022-07-01 18:48:49 -0700

    widgets routes updated to Andys way

--- routes/widgets.js
+++ routes/widgets.js
@@ -7,21 +7,29 @@
 
 const express = require('express');
 const router  = express.Router();
+const widgetQueries = require('../db/widget-queries');
 
-module.exports = (db) => {
-  router.get("/", (req, res) => {
-    let query = `SELECT * FROM widgets`;
-    console.log(query);
-    db.query(query)
-      .then(data => {
-        const widgets = data.rows;
-        res.json({ widgets });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
-  return router;
-};
+// GET /widgets/
+router.get('/', (req, res) => {
+  widgetQueries.getWidgets()
+    .then((widgets) => {
+      res.json(widgets);
+    })
+    .catch((error) => {
+      error.message
+    });
+});
+
+// GET /widgets/:id
+router.get('/:id', (req, res) => {
+  widgetQueries.getWidgetById(req.params.id)
+    .then((widget) => {
+      res.json(widget);
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+});
+
+module.exports = router;
+
