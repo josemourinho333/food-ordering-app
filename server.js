9c3107128c2265c2ac512a8c091a9b2fb3136ae1
Jesse Shen <jly.shen@gmail.com>
2022-07-03 20:08:23 +0000

    menu table

--- server.js
+++ server.js
@@ -28,15 +28,13 @@ app.use(
 app.use(express.static("public"));
 
 // Separated Routes for each Resource
-const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
-const orderRoutes = require('./routes/order');
+// const usersRoutes = require("./routes/users");
+// const widgetsRoutes = require("./routes/widgets");
+// const orderRoutes = require('./routes/order');
 const menuRoutes = require('./routes/menu');
 
 // Mount all resource routes
-app.use("/users", usersRoutes);
-app.use("/widgets", widgetsRoutes);
-app.use('/order', orderRoutes);
+// app.use("/users", usersRoutes);bootstrap.min.css
 app.use('/menu', menuRoutes);
 // Note: mount other resources here, using the same pattern above
 
