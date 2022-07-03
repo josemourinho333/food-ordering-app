d07a9c44ae0d8eb3388390701f686fe1f996e41a
ukflava <ukflava@gmail.com>
2022-07-02 19:35:38 -0700

    add AddItemToOrder

--- db/user-queries.js
+++ db/user-queries.js
@@ -32,7 +32,17 @@ const getAllOrdersByUserId = (id) => {
 };
 
 const getAllItemsInOrder = (id) => {
-  return db.query('SELECT order_items.* FROM order_items JOIN orders ON orders.id = order_items.order_id WHERE orders.id = 1;', [id])
+  return db.query('SELECT order_items.id, menu_items.name, order_items.quantity FROM order_items JOIN menu_items ON order_items.menu_item_id = menu_items.id JOIN orders ON orders.id = order_items.order_id WHERE orders.id = $1;', [id])
+    .then((response) => {
+      return response.rows;
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+};
+const AddItemToOrder = (orderID, itemID, quantity) => {
+  let vals = [orderID, itemID, quantity]
+  return db.query('INSERT INTO order_items ( order_id, menu_item_id, quantity)  VALUES  ( $1, $2, $3)', [vals])
     .then((response) => {
       return response.rows;
     })
@@ -44,7 +54,7 @@ const getAllItemsInOrder = (id) => {
 
 
 module.exports = {
-  getUsers, getUserById, getAllOrdersByUserId, getAllItemsInOrder,
+  getUsers, getUserById, getAllOrdersByUserId, getAllItemsInOrder, AddItemToOrder
 }
 
 // List of queries
