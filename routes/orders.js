9847b61e9810ce514123e5331147c845599d819d
josemourinho333 <yoo.phil92@gmail.com>
2022-07-02 17:15:43 -0700

    @Denis, peep the comments

--- routes/orders.js
+++ routes/orders.js
@@ -8,27 +8,19 @@ const ordersQueries = require('../db/orders-queries');
 // testing purposes
 const admin = true;
 
-// testing purposes II
-const order = {
-  id: 1,
-  user_id: 2,
-  order_placed: new Date(),
-  order_pickup: new Date() 
-}
-
 // get /orders/ - display list of all orders if admin, else redirect to /orders/:id
 router.get('/', (req, res) => {
   if (!admin) {
     console.log('hell nah you aint admin');
     return res.render('index');
   }
+  // need db/order queries
   ordersQueries.getAllOrders()
   .then((allOrders) => {
     const templateVars = {
       allOrders,
     }
     console.log('all orders', templateVars);
-    txtSend.orderConfirmed();
     res.render('orders', templateVars);
   })
   .catch((error) => {
