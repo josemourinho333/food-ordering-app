906926d41d1fa0b1a055a536504c511b9d63f0a4
josemourinho333 <yoo.phil92@gmail.com>
2022-07-02 17:01:22 -0700

    routes and twilio

--- routes/orders.js
+++ routes/orders.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const txtSend = require('../twilio/twilio-queries');
 
 // subject to change per Denis
 const ordersQueries = require('../db/orders-queries');
 
 // testing purposes
 const admin = true;
+
+// testing purposes II
+const order = {
+  id: 1,
+  user_id: 2,
+  order_placed: new Date(),
+  order_pickup: new Date() 
+}
 
 // get /orders/ - display list of all orders if admin, else redirect to /orders/:id
 router.get('/', (req, res) => {
@@ -19,13 +28,13 @@ router.get('/', (req, res) => {
       allOrders,
     }
     console.log('all orders', templateVars);
+    txtSend.orderConfirmed();
     res.render('orders', templateVars);
   })
   .catch((error) => {
     console.log(error.message);
   })
-})
-
+});
 
 module.exports = router;
 
