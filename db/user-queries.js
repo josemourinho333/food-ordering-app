46f32c377baaa5ba25923ce06e967917faf736f1
ukflava <ukflava@gmail.com>
2022-07-02 19:55:37 -0700

    add AddItemToOrder

--- db/user-queries.js
+++ db/user-queries.js
@@ -1,18 +1,18 @@
 // database connection to make queries possible
 const db = require('./db-connect');
 
-const getUsers = () => {
-  return db.query(`SELECT * FROM users;`)
-    .then((response) => {
-      return response.rows;
-    })
-    .catch((error) => {
-      console.log(error.message);
-    });
-};
+// const getUsers = () => {
+//   return db.query(`SELECT * FROM users;`)
+//     .then((response) => {
+//       return response.rows;
+//     })
+//     .catch((error) => {
+//       console.log(error.message);
+//     });
+// };
 
 const getUserById = (id) => {
-  return db.query('SELECT * FROM users WHERE users.id = $1;', [id])
+  return db.query(`SELECT * FROM users WHERE users.id = $1;`, [id])
     .then((response) => {
       return response.rows[0];
     })
@@ -22,7 +22,7 @@ const getUserById = (id) => {
 };
 
 const getAllOrdersByUserId = (id) => {
-  return db.query('SELECT orders.* FROM orders JOIN users ON users.id = orders.user_id WHERE users.id = $1;', [id])
+  return db.query(`SELECT orders.* FROM orders JOIN users ON users.id = orders.user_id WHERE users.id = $1;`, [id])
     .then((response) => {
       return response.rows;
     })
@@ -32,7 +32,7 @@ const getAllOrdersByUserId = (id) => {
 };
 
 const getAllItemsInOrder = (id) => {
-  return db.query('SELECT order_items.id, menu_items.name, order_items.quantity FROM order_items JOIN menu_items ON order_items.menu_item_id = menu_items.id JOIN orders ON orders.id = order_items.order_id WHERE orders.id = $1;', [id])
+  return db.query(`SELECT order_items.id, menu_items.name, order_items.quantity FROM order_items JOIN menu_items ON order_items.menu_item_id = menu_items.id JOIN orders ON orders.id = order_items.order_id WHERE orders.id = $1;`, [id])
     .then((response) => {
       return response.rows;
     })
@@ -42,7 +42,7 @@ const getAllItemsInOrder = (id) => {
 };
 const AddItemToOrder = (orderID, itemID, quantity) => {
   let vals = [orderID, itemID, quantity]
-  return db.query('INSERT INTO order_items ( order_id, menu_item_id, quantity)  VALUES  ( $1, $2, $3)', [vals])
+  return db.query(`INSERT INTO order_items ( order_id, menu_item_id, quantity)  VALUES  ( $1, $2, $3) RETURNING *;`, [vals])
     .then((response) => {
       return response.rows;
     })
