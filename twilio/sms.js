976fc44e732fbf9e001126935545f6c5908d057c
Jesse Shen <jly.shen@gmail.com>
2022-07-04 03:43:09 +0000

    fixed menu.ejs

--- twilio/sms.js
+++ twilio/sms.js
@@ -4,8 +4,8 @@ const twilio = require('twilio');
 const { accountSid, authToken } = require('./twilio-setup');
 
 const sms = new twilio(accountSid, authToken);
-const to = '+16049964504';
-const from = '+19804948591';
+const to = '+17786686850';
+const from = '+13254250499';
 
 module.exports = {
   sms,
--- twilio/twilio-setup.js
+++ twilio/twilio-setup.js
@@ -5,3 +5,6 @@ const twilioParams = {
 };
 
 module.exports = { twilioParams };
+
+
+
