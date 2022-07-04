7a80fe652b8e45594a192f4854c9dc8b08ea5784
josemourinho333 <yoo.phil92@gmail.com>
2022-07-03 20:31:31 -0700

    what i have so far

--- routes/orders.js
+++ routes/orders.js
@@ -4,9 +4,20 @@ const txtSend = require('../twilio/twilio-queries');
 
 // subject to change per Denis
 const ordersQueries = require('../db/orders-queries');
+// const { twilioParams } = require('../twilio/twilio-setup');
+// const { sms } = require('../twilio/sms');
 
 // testing purposes
 const admin = true;
+
+// When an order is placed the restaurant receives the order via SMS. The restaurant can then specify how long it will take to fulfill it. Once they provide this information, the website updates for the client and also notifies them via SMS.
+
+// testing purposes II
+const order = {
+  id: 123,
+  user_id: 2,
+  order_placed: new Date(),
+}
 
 // get /orders/ - display list of all orders if admin, else redirect to /orders/:id
 router.get('/', (req, res) => {
@@ -21,11 +32,47 @@ router.get('/', (req, res) => {
       allOrders,
     }
     console.log('all orders', templateVars);
+    /// testing
+    txtSend.orderConfirmed(order);
+    // txtSend.orderReady(order);
+    ///
     res.render('orders', templateVars);
   })
   .catch((error) => {
     console.log(error.message);
   })
+});
+
+//TESTING
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+// should send order info to restaurant via sms
+router.post('/new', (req, res) => {
+  console.log('**** new order came in ****');
+
+  txtSend.newOrder(req.body);
+  // send off req.body to Den
+  ordersQueries.newOrder(req.body)
+    .then()
+
+  if (req.body.statusSent) {
+    txtSend.newOrder()
+  } else {
+    const ownerConfirm = req.body.Body;
+    console.log('inside confirming', req.body);
+    if (ownerConfirm) {
+      // Start our TwiML response.
+      const reply = new MessagingResponse();
+      // // Add a text message.
+      const message = reply.message();
+      message.body('from message body');
+      res.end(reply.toString());
+
+      // txtSend.orderConfirmed(order, () => {
+      //   res.send(order);
+      // })
+    }
+  }
 });
 
 module.exports = router;
--- twilio/twilio-queries.js
+++ twilio/twilio-queries.js
@@ -1,11 +1,31 @@
 // you will need to require this file inside where you want to use twilio
-
 const { sms, to, from } = require('./sms');
 
-const orderConfirmed = () => {
+const orderConfirmed = (order, callback) => {
+
+  const prepTime = 45;
+  const ETA = order.order_placed;
+  ETA.setMinutes(ETA.getMinutes() + 45);
+  const etaHr = ETA.getHours();
+  const etaMin = ETA.getMinutes();
+
   sms.messages
     .create({
-      body: 'Your order is confirmed 😊',
+      body: `Hi ${order.id},
+
+      your order is confirmed and will be ready in ${prepTime} minutes at ${(etaHr > 12) ? etaHr - 12 : etaHr}:${etaMin} ${(etaHr > 12) ? 'PM' : 'AM'}. 😊`,
+      to: to,
+      from: from
+    })
+    .then((message) => {console.log(message)})
+    .then(callback(order))
+    .catch((error) => {console.log(error.message)});
+};
+
+const orderReady = (order) => {
+  sms.messages
+    .create({
+      body: `Your order #${order.id} is ready for pick up, thank you! 😎`,
       to: to,
       from: from
     })
@@ -13,10 +33,12 @@ const orderConfirmed = () => {
     .catch((error) => {console.log(error.message)});
 };
 
-const orderReady = () => {
+const newOrder = (order) => {
+  console.log('**** Inside newOrder ****');
+  console.log('you can see order', order);
   sms.messages
     .create({
-      body: 'Your order is ready for pick up now! 😎',
+      body: `New order came in. Reply to confirm the order and let the customer know when their order will be ready.`,
       to: to,
       from: from
     })
@@ -27,4 +49,5 @@ const orderReady = () => {
 module.exports = {
   orderConfirmed,
   orderReady,
+  newOrder,
 };
