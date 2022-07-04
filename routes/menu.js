75984dff870f6d92fb8a3eac0126e12ac1025351
josemourinho333 <yoo.phil92@gmail.com>
2022-07-04 11:29:31 -0700

    Now handles add to cart

--- routes/menu.js
+++ routes/menu.js
@@ -4,21 +4,43 @@ const router = express.Router();
 // subject to change per Denis
 const menuQueries = require('../db/menu-queries');
 
-// get /menu/ - display menu items
+// get /menu/ - will create new order and display menu items.
 router.get('/', (req, res) => {
-  // need db/menu queries
-  menuQueries.getMenuItems()
-    .then((menuItems) => {
-      const templateVars = {
-        menuItems,
-      }
-      res.render('menu', templateVars);
+  // db query to create new order - needs changed once Denis is done
+  menuQueries.addNewOrder(req.session.user_id)
+    .then((newOrder) => {
+      menuQueries.getMenuItems()
+        .then((menuItems) => {
+          const templateVars = {
+            newOrder,
+            menuItems,
+          }
+          console.log('templatevars', templateVars.newOrder);
+          // needs to menu/orders/:id here and pass data.
+          res.render(`menu`, templateVars)
+        })
     })
-    .catch((error) => {
-      console.log(error.message);
-    });
+    .catch((error) => {console.log(error)});
+});
+
+// post /menu/orders/:id - add to cart posts.
+router.post('/', (req, res) => {
+  console.log('reqbody', req.body);
+  // group req.body info and req.params.id into one obj.
+  const orderItem = {
+    quantity: req.body.quantity,
+    itemId: req.body.itemID,
+    // needs to be replaced with req.params.id instead of number.
+    orderId: 30
+  }
+  // db query to insert this obj into orders_items
+  menuQueries.addNewItem(orderItem)
+    //should get back inserted order_item but take the order_id.
+    .then(() => {
+      // here need to render again with the same /menu/order/:id
+      res.render(`menu`);
+    })
+    .catch((error) => {console.log(error.message)});
 });
 
 module.exports = router;
-
-// :
