80d537b9575981c4d1a83af9c2417a1b538afea3
ukflava <ukflava@gmail.com>
2022-07-04 18:15:22 -0700

    add getTotalInOrder with sum of order to queries

--- db/user-queries.js
+++ db/user-queries.js
@@ -1,6 +1,8 @@
 // database connection to make queries possible
 const db = require('./db-connect');
 
+
+//USER QUERIES
 const getUsers = () => {
   return db.query(`SELECT * FROM users;`)
     .then((response) => {
@@ -41,35 +43,6 @@ const getAllSentOrdersAsAdmin = () => {
     });
 };
 
-const getAllItemsInOrder = (id) => {
-  return db.query(`SELECT orders.id AS orderID, order_items.id AS itemNumber, menu_items.name, menu_items.price, order_items.quantity FROM order_items JOIN menu_items ON order_items.menu_item_id = menu_items.id JOIN orders ON orders.id = order_items.order_id WHERE orders.id = $1;`, [id])
-    .then((response) => {
-      return response.rows;
-    })
-    .catch((error) => {
-      console.log(error.message);
-    });
-};
-const addItemToOrder = (item) => {
-  let vals = [item.orderId, item.itemId, item.quantity]
-  return db.query(`INSERT INTO order_items ( order_id, menu_item_id, quantity)  VALUES  ( $1, $2, $3) RETURNING *;`, vals)
-    .then((response) => {
-      return response.rows;
-    })
-    .catch((error) => {
-      console.log(error.message);
-    });
-};
-const createOrder = (id) => {
-  let vals = [id]
-  return db.query(`INSERT INTO orders ( user_id)  VALUES  ($1) RETURNING *;`, vals)
-    .then((response) => {
-      return response.rows[0];
-    })
-    .catch((error) => {
-      console.log(error.message);
-    });
-};
 
 // const setPickUpTimeToOrder = (orderID, timeInMinutes) => {
 //   let vals = [orderID, timeInMinutes]
@@ -98,6 +71,38 @@ const updateStatusOwnerConfirm = (orderID, timeInMinutes) => {
   return db.query(`UPDATE orders SET status_finished = 'true', time_confirmed = CURRENT_TIMESTAMP, time_of_pickup = CURRENT_TIMESTAMP + INTERVAL '$2 minutes'   WHERE orders.id = $1 RETURNING *;`, vals)
     .then((response) => {
       return response.rows;
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+};
+
+//  ORDER QUERIES
+
+const getAllItemsInOrder = (id) => {
+  return db.query(`SELECT orders.id AS orderID, order_items.id AS itemNumber, menu_items.name, menu_items.price, order_items.quantity FROM order_items JOIN menu_items ON order_items.menu_item_id = menu_items.id JOIN orders ON orders.id = order_items.order_id WHERE orders.id = $1;`, [id])
+    .then((response) => {
+      return response.rows;
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+};
+const addItemToOrder = (item) => {
+  let vals = [item.orderId, item.itemId, item.quantity]
+  return db.query(`INSERT INTO order_items ( order_id, menu_item_id, quantity)  VALUES  ( $1, $2, $3) RETURNING *;`, vals)
+    .then((response) => {
+      return response.rows;
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+};
+const createOrder = (id) => {
+  let vals = [id]
+  return db.query(`INSERT INTO orders ( user_id)  VALUES  ($1) RETURNING *;`, vals)
+    .then((response) => {
+      return response.rows[0];
     })
     .catch((error) => {
       console.log(error.message);
