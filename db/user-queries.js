8bdfb8daea35ff8b0f993272359767e18410b37a
ukflava <ukflava@gmail.com>
2022-07-06 14:56:26 -0700

    change seeds, change queries to fix bug with orders page

--- db/user-queries.js
+++ db/user-queries.js
@@ -84,7 +84,8 @@ const getAllSentOrdersAsAdmin = () => {
 //  ORDER QUERIES
 
 const getAllItemsInOrder = (id) => {
-  return db.query(`SELECT orders.id AS orderID, order_items.menu_item_id AS itemNumber, menu_items.name, menu_items.price, order_items.quantity FROM order_items JOIN menu_items ON order_items.menu_item_id = menu_items.id JOIN orders ON orders.id = order_items.order_id WHERE orders.id = $1;`, [id])
+  // return db.query(`SELECT orders.id AS orderID, order_items.menu_item_id AS itemNumber, menu_items.name, menu_items.price, order_items.quantity FROM order_items JOIN menu_items ON order_items.menu_item_id = menu_items.id JOIN orders ON orders.id = order_items.order_id WHERE orders.id = $1;`, [id])
+  return db.query(`SELECT orders.id AS orderID, order_items.menu_item_id AS itemNumber, MIN(menu_items.name), MIN(menu_items.price), SUM(order_items.quantity) FROM order_items JOIN menu_items ON order_items.menu_item_id = menu_items.id JOIN orders ON orders.id = order_items.order_id WHERE orders.id = $1 GROUP BY itemNumber, orders.id;`, [id])
 
     .then((response) => {
       return response.rows;
